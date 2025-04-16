5324b2c95f28254eb3cebc684ad2b3b67a393cf4
galodelia <gmdelia@mail.austral.edu.ar>
2025-04-16 13:52:47 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,2 +1,9 @@
 def leap_year():
-    print("TO DO")
+    a = int(input("Ingrese un año: "))
+    b = a % 4
+    c = a % 400
+    d = a % 100
+    if (b == 0 and d != 0) or c == 0: 
+        print(f"El año {a} es bisiesto")
+    else: 
+        print(f"El año {a} no es bisiesto")
